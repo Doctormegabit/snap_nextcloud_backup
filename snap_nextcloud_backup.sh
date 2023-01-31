f029a296a9434daeb769acf1f326410d59d6c194
Doctormegabit <73620913+Doctormegabit@users.noreply.github.com>
2023-02-01 02:05:46 +0400

    Update snap_nextcloud_backup.sh
    thes bash scypt can backup you nextcloud and sending mail to you. You can keep 3 copes of backups

--- snap_nextcloud_backup.sh
+++ snap_nextcloud_backup.sh
@@ -1,74 +1,76 @@
+#!/bin/bash
 # Backup for Nextcloud
+echo "Backups of nextcloud data and apps is started" | mail -s "Backups of nextcloud" mail@gmail.com # for sendin mail you can use postfix for example
+#exec 1>/path to log file/backup.log
+#exec 2>/path to erors of backup/ERRORS.log
 #
 ## VARIABLES
-DATADIR=/media/nas/RAID-1/nextcloud/data  #by default
-DESTDIR=/media/nas/RAID-1/nextcloud_backups_tgz
+DATADIR=/full path to data dir/data  #by default
+DESTDIR=/full path to destination dir/nextcloud_backups_tgz
 
 # How many snap backup do you want to keep?
-NUMBCK='6'
+NUMBCK='3'
 
 ## CONS
 NAME=$(uname -n)
 DATE=$(date +'%Y-%m-%d')
-BCKDIR=/var/snap/nextcloud/common/backups/
+BCKDIR=/var/snap/nextcloud/common/backups
 
 ## SUB
 info()
 {
-	echo "${NAME}|$(date +'%Y-%m-%d %H:%M:%S INFO: ')${@}" 1>&2
+        echo "${NAME}|$(date +'%Y-%m-%d %H:%M:%S INFO: ')${@}" 1>&2
 } # end info
 
-info "Checking Backup destination"
+echo "Checking Backup destination"
 if [ -d ${DESTDIR} ];
 then
-    info "Destination exist"
+    echo "Destination exist"
 else
-    info "Creating destination folder"
+    echo "Creating destination folder"
     mkdir -p ${DESTDIR}/snap
     chmod -r 700 ${DESTDIR}
 fi
 
 
 # backup Nextcloud
-info "Backing up Snap folder"
+echo "Backing up Snap folder run nextcloud.export -abc"
 /snap/bin/nextcloud.export -abc
 if [ $? -eq 0 ];
-#if [[ 0 == 0 ]];
 then
     LASTBCK=`ls -tr -1 ${BCKDIR} | tail -1`
-    info "Archiving Snap confing backup"
-#    mkdir -p ${DESTDIR}/snap/${LASTBCK}/
-#    chmod -r 700  ${DESTDIR}/snap/${LASTBCK}
-#    tar -zcf ${DESTDIR}/snap/${LASTBCK}/${LASTBCK}\.tar.gz ${BCKDIR}/${LASTBCK}
-    tar -zcf ${DESTDIR}/snap/${LASTBCK}\.tar.gz ${BCKDIR}/${LASTBCK}
+    echo "Archiving Snap confing backup"
+    tar cvpzf ${DESTDIR}/snap/${LASTBCK}\.tgz ${BCKDIR}/${LASTBCK}
 
-    info "Removing local backup"
-#    rm -Rf ${BCKDIR}/${LAST}
+    echo "Removing local backup"
+#    rm -Rf ${BCKDIR}/${LAST} # if you uncoment thet, you removing backups dir evry time. i do the simlink on /var/snap/nextcloud/common/backups to external drive
 else
     info "Nextcloud export failed, exiting..."
     exit 1
 fi
 
-info " rotate snap backup, keep last ${NUMBCK}"
-ls -tp ${BCKDIR} | tail -n +$((${NUMBCK} + 1 )) | xargs -I {} rm -rf -- ${BCKDIR}{}
-info ${NUMBCK} "IS Keeped"
-info " backup Data"
+echo " rotate snap backup, keep last ${NUMBCK}"
+ls -tp ${BCKDIR} | tail -n +$((${NUMBCK} + 1 )) | xargs -I {} rm -rf -- ${BCKDIR}/{}
+echo ${NUMBCK} "IS Keeped"
+echo " backup Data"
 if [ -d ${DATADIR} ];
 then
     cd ${DATADIR}
-    info "Stopping Nextcloud"
+    echo "Stopping Nextcloud"
     snap stop nextcloud
-    info "Backing up Data folder"
-    rsync -azP --delete ${DATADIR} ${DESTDIR}
-    info "Starting Nextcloud"
+    echo "Backing up Data folder"
+    rsync -azP --delete --progress ${DATADIR} ${DESTDIR}
+    echo "Starting Nextcloud"
     snap start nextcloud
 else
     info "Data Directory doesn't exist, exiting..."
     exit 1
 fi
 
-info " rotate snap backup, keep last ${NUMBCK}"
-ls -tp ${DESTDIR}/snap/ | tail -n +$((${NUMBCK} + 1 )) | xargs -I {} rm -rf -- ${DESTDIR}/snap/{}
-info ${NUMBCK} "IS Keeped"
+echo " rotate snap archined backup, keep last ${NUMBCK}"
+ls -tp ${DESTDIR}/snap/ | tail -n +$((${NUMBCK} + 1 )) | xargs -I {} rm -- ${DESTDIR}/snap/{}
+echo ${NUMBCK} "IS Keeped"
 
-info "Backup Completed on " ${DESTDIR}
+echo "Backup Completed on " ${DESTDIR}
+#exec 0
+echo "Backups of nextcloud data and apps was secessfull" | mail -s "Backups of nextcloud" mail@gmail.com
